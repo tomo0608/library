6cf46b1263b37246485dfef509ddd6d8b9e0464a
sugiyama tomohiro <totatari0608@gmail.com>
2023-03-02 17:43:31 +0900

    namespace tomo0608

---
+++ SWAG.test.cpp
@@ -0,0 +1,84 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/queue_operate_all_composite"
+
+#include<vector>
+#include<stack>
+#include<cassert>
+
+namespace tomo0608 {
+    template<class Monoid, Monoid(*op)(Monoid, Monoid), Monoid(*e)()> struct SWAG {
+        SWAG():l(0), r(0), last_i(0), last_j(0), front(e()), v(0), back() {}
+        explicit SWAG(std::vector<Monoid>& _v):l(0), r(0), last_i(0), last_j(0), front(e()), v(_v.size()), back() {
+            std::copy(_v.begin(), _v.end(), v.begin());
+        }
+        void push_back(const Monoid& c) {
+            v.push_back(c);
+        }
+        size_t size() {
+            return v.size();
+        }
+        Monoid fold(size_t i, size_t j) { //[l, r)
+            assert(i < j && j <= v.size());
+            assert(last_i <= i && last_j <= j);
+            last_i = i, last_j = j;
+            while (r < j)front = op(front, v[r++]);
+            while (l < i) {
+                if (back.empty()) {
+                    Monoid tmp = e();
+                    for (size_t u = r; u-- > l; ) {
+                        tmp = op(v[u], tmp);
+                        back.emplace(tmp);
+                    }
+                    front = e();
+                }
+                back.pop();
+                ++l;
+            }
+            if (back.empty())return front;
+            return op(back.top(), front);
+        }
+    private:
+        std::vector<Monoid> v;
+        Monoid front;
+        std::stack<Monoid> back;
+        size_t l, r, last_i, last_j;
+    };
+}
+
+#include<iostream>
+using namespace std;
+#define ll long long
+const ll MOD = 998244353;
+#define S pair<ll,ll>
+S op(S x, S y) {
+    S z;
+    z.first = x.first * y.first % MOD;
+    z.second = (y.first * x.second + y.second) % MOD;
+    return z;
+}
+S e() { return { 1, 0 }; }
+int main() {
+    int q;cin >> q;
+    int l = 0, r = 0;
+    tomo0608::SWAG<S, op, e> swag;
+    while (q--) {
+        int i;cin >> i;
+        if (i == 0) {
+            ll a, b;cin >> a >> b;
+            swag.push_back(S(a, b));
+            r++;
+        }
+        if (i == 1) {
+            l++;
+        }
+        if (i == 2) {
+            ll x;cin >> x;
+            if (l == r) {
+                cout << x << endl;
+                continue;
+            }
+            auto [a, b] = swag.fold(l, r);
+            ll fx = (a * x + b) % MOD;
+            cout << fx << endl;
+        }
+    }
+}
--- unionfind.test.cpp
+++ unionfind.test.cpp
@@ -1,27 +1,29 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/unionfind"
 #include<bits/stdc++.h>
 
-struct unionfind{
-    public:
-        unionfind(): _n(0){}
-        unionfind(int n):  _n(n),parent_or_size(n, -1){}
+namespace tomo0608 {
 
-        int find(int a){
+    struct unionfind {
+    public:
+        unionfind(): _n(0) {}
+        unionfind(int n): _n(n), parent_or_size(n, -1) {}
+
+        int find(int a) {
             assert(0 <= a && a < _n);
-            if(parent_or_size[a] < 0)return a;
+            if (parent_or_size[a] < 0)return a;
             return parent_or_size[a] = find(parent_or_size[a]);
         }
 
-        bool same(int a, int b){
+        bool same(int a, int b) {
             assert(0 <= a && a < _n && 0 <= b && b < _n);
             return find(a) == find(b);
         }
 
-        bool merge(int a, int b){
+        bool merge(int a, int b) {
             assert(0 <= a && a < _n && 0 <= b && b < _n);
             int x = find(a), y = find(b);
-            if(x == y)return true;
-            if(-parent_or_size[x] < -parent_or_size[y])std::swap(x,y);
+            if (x == y)return true;
+            if (-parent_or_size[x] < -parent_or_size[y])std::swap(x, y);
             parent_or_size[x] += parent_or_size[y];
             parent_or_size[y] = x;
             return false;
@@ -30,16 +32,16 @@ struct unionfind{
     private:
         int _n;
         std::vector<int> parent_or_size;
-};
+    };
+} // namespace tomo0608
 
-// https://old.yosupo.jp/problem/unionfind
 using namespace std;
-int main(){
-    int n,q;cin >> n >> q;
-    unionfind uf(n);
-    while(q--){
-        int t,u,v;cin >> t >> u >> v;
-        if(t)cout << uf.same(u, v) << endl;
+int main() {
+    int n, q;cin >> n >> q;
+    tomo0608::unionfind uf(n);
+    while (q--) {
+        int t, u, v;cin >> t >> u >> v;
+        if (t)cout << uf.same(u, v) << endl;
         else uf.merge(u, v);
     }
     return 0;
