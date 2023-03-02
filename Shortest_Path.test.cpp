2be5a75eb71b1642721902ab45720ae01f3c139a
sugiyama tomohiro <totatari0608@gmail.com>
2023-03-02 17:46:39 +0900

    namespace tomo0608

--- Shortest_Path.test.cpp
+++ Shortest_Path.test.cpp
@@ -7,55 +7,59 @@
 #include<algorithm>
 #include<iostream>
 
-class Weighted_Digraph {
-public:
-    std::vector<std::vector<std::pair<long long, int>>> Adjacency_List, Transpose;
-    unsigned number_of_nodes;
-    Weighted_Digraph(unsigned n): number_of_nodes(n), Adjacency_List(n), Transpose(n) {};
+namespace tomo0608 {
 
-    void add_edge(int from, int to, unsigned long long weight) {
-        assert(0 <= from && from < number_of_nodes);
-        assert(0 <= to && to < number_of_nodes);
-        Adjacency_List[from].emplace_back(weight, to);
-        Transpose[to].emplace_back(weight, from);
-    }
+    class Weighted_Digraph {
+    public:
+        std::vector<std::vector<std::pair<long long, int>>> Adjacency_List, Transpose;
+        unsigned number_of_nodes;
+        Weighted_Digraph(unsigned n): number_of_nodes(n), Adjacency_List(n), Transpose(n) {};
 
-    std::pair<std::vector<long long>, std::vector<int>> Dijkstra(int start) { // startを始点とした最短距離をダイクストラ法で求める, (最短距離のリスト, 最短経路木の親の情報)
-        std::vector<long long> Dist(number_of_nodes, -1);
-        std::vector<int> par(number_of_nodes, -1);
-        using P = std::pair<long long, int>;
-        std::priority_queue<P, std::vector<P>, std::greater<P>> pq;
-        Dist[start] = 0;
-        pq.emplace(0, start);
-        while (!pq.empty()) {
-            auto [d, u] = pq.top();
-            pq.pop();
-            if (Dist[u] < d)continue;
-            for (auto& [w, v] : Adjacency_List[u]) {
-                if (Dist[v] == -1 || Dist[v] > Dist[u] + w) {
-                    Dist[v] = Dist[u] + w;
-                    pq.emplace(Dist[v], v);
-                    par[v] = u;
+        void add_edge(int from, int to, unsigned long long weight) {
+            assert(0 <= from && from < number_of_nodes);
+            assert(0 <= to && to < number_of_nodes);
+            Adjacency_List[from].emplace_back(weight, to);
+            Transpose[to].emplace_back(weight, from);
+        }
+
+        std::pair<std::vector<long long>, std::vector<int>> Dijkstra(int start) { // startを始点とした最短距離をダイクストラ法で求める, (最短距離のリスト, 最短経路木の親の情報)
+            std::vector<long long> Dist(number_of_nodes, -1);
+            std::vector<int> par(number_of_nodes, -1);
+            using P = std::pair<long long, int>;
+            std::priority_queue<P, std::vector<P>, std::greater<P>> pq;
+            Dist[start] = 0;
+            pq.emplace(0, start);
+            while (!pq.empty()) {
+                auto [d, u] = pq.top();
+                pq.pop();
+                if (Dist[u] < d)continue;
+                for (auto& [w, v] : Adjacency_List[u]) {
+                    if (Dist[v] == -1 || Dist[v] > Dist[u] + w) {
+                        Dist[v] = Dist[u] + w;
+                        pq.emplace(Dist[v], v);
+                        par[v] = u;
+                    }
                 }
             }
+            return std::make_pair(Dist, par);
         }
-        return std::make_pair(Dist, par);
+    };
+} // namespace tomo0608
+
+namespace tomo0608 {
+    std::vector<int> restore_path(std::vector<int> par, int goal) {
+        std::vector<int> path = { goal };
+        while (par[path.back()] != -1)path.emplace_back(par[path.back()]);
+        std::reverse(path.begin(), path.end());
+        return path;
     }
-};
-
-std::vector<int> restore_path(std::vector<int> par, int goal) {
-    std::vector<int> path = { goal };
-    while (par[path.back()] != -1)path.emplace_back(par[path.back()]);
-    std::reverse(path.begin(), path.end());
-    return path;
-}
-
+} // namespace tomo0608
 
 using namespace std;
 
 int main() {
     unsigned n, m, s, t;cin >> n >> m >> s >> t;
-    Weighted_Digraph g(n);
+    tomo0608::Weighted_Digraph g(n);
     for (int i = 0; i < m; i++) {
         unsigned a, b;cin >> a >> b;
         long long c;cin >> c;
@@ -67,7 +71,7 @@ int main() {
         cout << -1 << endl;
         return 0;
     }
-    auto path = restore_path(par, t);
+    auto path = tomo0608::restore_path(par, t);
     cout << dist[t] << ' ' << path.size() - 1 << endl;
     for (int i = 0; i < path.size() - 1; i++)cout << path[i] << ' ' << path[i + 1] << endl;
     return 0;
--- static_range_sum.test.cpp
+++ static_range_sum.test.cpp
@@ -3,26 +3,28 @@
 #include<vector>
 #include<cassert>
 
-template <class S, S(*op)(S, S), S(*e)(), S(*inv)(S)>
-struct static_range_sum {
-    int n;
-    std::vector<S> range_sum;
+namespace tomo0608 {
 
-    static_range_sum(std::vector<S>& a) {
-        n = (int)a.size();
-        range_sum.resize(n + 1);
-        range_sum[0] = e();
-        for (int i = 0; i < n; i++) {
-            range_sum[i + 1] = op(range_sum[i], a[i]);
+    template <class S, S(*op)(S, S), S(*e)(), S(*inv)(S)>
+    struct static_range_sum {
+        int n;
+        std::vector<S> range_sum;
+
+        static_range_sum(std::vector<S>& a) {
+            n = (int)a.size();
+            range_sum.resize(n + 1);
+            range_sum[0] = e();
+            for (int i = 0; i < n; i++) {
+                range_sum[i + 1] = op(range_sum[i], a[i]);
+            }
         }
-    }
 
-    inline S query(int l, int r) const {
-        assert(0 <= l && l < r&& r <= n);
-        return op(inv(range_sum[l]), range_sum[r]);
-    }
-};
-
+        inline S query(int l, int r) const {
+            assert(0 <= l && l < r && r <= n);
+            return op(inv(range_sum[l]), range_sum[r]);
+        }
+    };
+}
 using S = long long;
 
 S op(S x, S y) { return x + y; }
@@ -38,7 +40,7 @@ int main() {
     std::cin >> n >> q;
     std::vector<long long> a(n);
     for (int i = 0; i < n; i++)std::cin >> a[i];
-    static_range_sum<S, op, e, inv> srs(a);
+    tomo0608::static_range_sum<S, op, e, inv> srs(a);
     for (int i = 0; i < q; i++) {
         int l, r;
         std::cin >> l >> r;
